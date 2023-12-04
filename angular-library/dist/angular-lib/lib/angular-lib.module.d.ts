d185775c0c0051a2c28fa8d6f458de4f53dae3aa
dextrarsilva <dextra.rsilva@creditas.com>
2023-12-04 10:03:22 -0300

    Add dist

--- angular-library/dist/angular-lib/lib/angular-lib.module.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./components/button/button.module";
-export declare class AngularLibModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<AngularLibModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<AngularLibModule, never, [typeof i1.ButtonModule], [typeof i1.ButtonModule]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<AngularLibModule>;
-}
--- angular-library/dist/angular-lib/lib/components/button/button.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class ButtonComponent {
-    variant: 'primary' | 'secundary';
-    size: 'sm' | 'md' | 'lg';
-    static ɵfac: i0.ɵɵFactoryDeclaration<ButtonComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ButtonComponent, "lib-button", never, { "variant": "variant"; "size": "size"; }, {}, never, ["*"]>;
-}
